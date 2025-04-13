b8d37b20d82a39f7e3f504dc564d6d0a1adf068a
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-04-13 21:17:01 +0530

    backend

--- src/main/java/fusionIQ/AI/V2/fusionIq/service/DataSourceConfig.java
+++ src/main/java/fusionIQ/AI/V2/fusionIq/service/DataSourceConfig.java
@@ -22,7 +22,7 @@ public class DataSourceConfig {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl("jdbc:mysql://prod.db.fusioniq.com:3306/prod?createDatabaseIfNotExist=true");
         config.setUsername("admin");
-        config.setPassword("TnnlIAu#DyRD6<g2]3B6$Rl7Zy)d");
+        config.setPassword("hX9!Z4<5F?Aeg#rv0KB0u5SPR]1t");
         config.addDataSourceProperty("connectionInitSql", "SET GLOBAL max_allowed_packet=134217728"); // 128 MB
         return new HikariDataSource(config);
     }
